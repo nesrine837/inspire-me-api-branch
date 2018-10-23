256bcc9e095544d46116f81d288c82ce240bb363
Phabiann-Ross De Carvalho <cervantes1234@live.com>
2018-10-23 13:09:27 +0200

    added quotees to api routes

--- app/Http/Controllers/v1/QuoteesController.php
+++ app/Http/Controllers/v1/QuoteesController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\v1;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class QuoteesController extends Controller
 {
-    //
+    public function index()
+    {
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::resource('/v1/quotes', v1\QuotesController::class);
+Route::resource('/v1/quotees', v1\QuoteesController::class);
