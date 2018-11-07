a889de0a6a6853fd84ff50f5672b80f92a1158de
Phabiann-Ross De Carvalho <cervantes1234@live.com>
2018-11-07 14:31:56 +0200

    added cors middleware to api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,8 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            'cors'
+
         ],
     ];
 
@@ -60,6 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
