b6f0ad01926fd376e784e3f4a192046d2779babb
Phabiann-Ross De Carvalho <cervantes1234@live.com>
2018-11-07 14:32:58 +0200

    moved mailer route function to its own controller

---
+++ app/Http/Controllers/v1/MailerController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use Illuminate\Http\Request;
+use App\Mail\NewProject;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
+
+class MailerController extends Controller
+{
+    protected $rules = [
+        'name' => 'required|min:2|max:40',
+        'email' => 'required|email',
+        'message' => 'required|max:500'
+    ];
+
+    public function newProject(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:2|max:40',
+            'email' => 'required|email',
+            'message' => 'required|max:500'
+        ]);
+
+        $data = $request->input();
+        Storage::put('log.txt', $request);
+        \Mail::to('phabiannthepoet@gmail.com')->send(new NewProject($data));
+
+        return response([], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Mail\NewProject;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,9 +24,4 @@ Route::resource('/v1/quotees', v1\QuoteesController::class);
 Route::resource('/v1/nationalities', v1\NationalitiesController::class);
 Route::resource('/v1/professions', v1\ProfessionsController::class);
 Route::resource('/v1/categories', v1\CategoriesController::class);
-Route::post('v1/mailer', function (Request $request) {
-    $data = $request->input();
-    fwrite()
-    dd($request);
-    \Mail::to('phabiannthepoet@gmail.com')->send(new NewProject($request->input()));
-});
+Route::post('v1/mailer', 'v1\MailerController@newProject');
