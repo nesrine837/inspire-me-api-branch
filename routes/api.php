67fa71ad01e581ad27275176f3188881ab10182e
Phabiann-Ross De Carvalho <cervantes1234@live.com>
2018-11-21 22:38:13 +0200

    cleaned up route excludes

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,22 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+$excludedRoutes = ['except' =>
+                    ['create', 'edit']];
+
 Route::redirect('/', 'https://github.com/Red-Mountain-Dev/inspire-me-api', 301);
-Route::resource('/v1/quotes', v1\QuotesController::class);
-Route::resource('/v1/quotees', v1\QuoteesController::class);
-Route::resource('/v1/nationalities', v1\NationalitiesController::class);
-Route::resource('/v1/professions', v1\ProfessionsController::class);
-Route::resource('/v1/categories', v1\CategoriesController::class);
+
+Route::resource('/v1/quotes', v1\QuotesController::class, $excludedRoutes);
+
+Route::resource('/v1/quotees', v1\QuoteesController::class, $excludedRoutes);
+
+Route::resource('/v1/nationalities', v1\NationalitiesController::class, $excludedRoutes);
+
+Route::resource('/v1/professions', v1\ProfessionsController::class, $excludedRoutes);
+
+Route::resource('/v1/categories', v1\CategoriesController::class, $excludedRoutes);
+
 Route::post('v1/mailer', 'v1\MailerController@newProject');
 Route::fallback(function () {
     $data = [];
