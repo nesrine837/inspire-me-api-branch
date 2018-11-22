5dc0e2e3a7c870e59d3a9184cc2dd0b1ec9b8a4b
Phabiann-Ross De Carvalho <cervantes1234@live.com>
2018-11-22 12:47:58 +0200

    added store function with validation to professions controller

--- app/Http/Controllers/v1/ProfessionsController.php
+++ app/Http/Controllers/v1/ProfessionsController.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers\v1;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\v1\ProfessionsService;
+use Validator;
+
+use App\Profession;
 
 class ProfessionsController extends Controller
 {
     protected $professionsService;
+    protected $rules = [
+        'profession_name' => 'required|unique:professions,profession_name',
+
+    ];
 
     public function __construct(ProfessionsService $service)
     {
@@ -27,6 +34,23 @@ class ProfessionsController extends Controller
         $professions = $this->professionsService->getProfessions($parameters);
         return response()->json($professions);
     }
+
+    public function store(Request $request)
+    {
+        $validation = Validator::make($request->input(), $this->rules);
+        if ($validation->fails()) {
+            return response()->json($validation->messages(), 400);
+        }
+
+        $profession = new Profession;
+
+        $profession->profession_name = $request->input('profession_name');
+
+        $profession->save();
+
+        return response()->json($profession, 201);
+    }
+
     public function destroy($id)
     {
         Profession::where('id', $id)->firstOrFail()->delete();
