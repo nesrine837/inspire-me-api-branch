5ea4f66547654066d0fd0e990d6908d1d53ac79f
Phabiann-Ross De Carvalho <cervantes1234@live.com>
2018-11-22 18:01:05 +0200

    added auth middleware and passport controller

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Auth::routes();
+
+Route::post('login', 'PassportController@login');
+Route::post('register', 'PassportController@register');
 
 $excludedRoutes = ['except' =>
                     ['create', 'edit']];
